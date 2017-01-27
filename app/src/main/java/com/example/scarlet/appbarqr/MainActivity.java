21d7b49e83482b4eaf640a47d457fa3c1f38d498
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-01-27 21:46:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/scarlet/appbarqr/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.scarlet.appbarqr;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+private ZXingScannerView zXingScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnClickbtn(View view){
+        zXingScannerView=new ZXingScannerView(this);
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.d("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+       // zXingScannerView.r
+
+    }
+}
